5dad30f3a0e94daa22d0b2c2791aceee538a0d75
Kole Davis <koledavis@MacBook-Pro.local>
2021-11-11 19:22:04 -0700

    something appears

---
+++ scripts/script.js
@@ -0,0 +1,68 @@
+const characters = [
+    {
+        name: 'Balrog of Morgoth',
+        race: 'Beast',
+        class: 'Druid',
+        gender: 'Unknown',
+        strength: '12',
+        dexterity: '14',
+        constitution: '10',
+        intelligence: '18',
+        wisdom: '12',
+        charisma: '9'
+    },
+    {
+        name: 'Gandalf the Grey',
+        race: 'Maiar',
+        class: 'Wizard',
+        gender: 'Male',
+        strength: '11',
+        dexterity: '11',
+        constitution: '18',
+        intelligence: '11',
+        wisdom: '10',
+        charisma: '13'
+    },
+    {
+        name: 'Aragorn son of Arathorn',
+        race: 'Numenorean',
+        class: 'Ranger',
+        gender: 'Male',
+        strength: '12',
+        dexterity: '12',
+        constitution: '12',
+        intelligence: '15',
+        wisdom: '7',
+        charisma: '16'
+    },
+    {
+        name: 'Legolas',
+        race: 'Elf',
+        class: 'Ranger',
+        gender: 'Male',
+        strength: '7',
+        dexterity: '10',
+        constitution: '17',
+        intelligence: '9',
+        wisdom: '14',
+        charisma: '18'
+    }
+]
+
+function showCharacters(n, r, c, g) {
+    // create section
+    const section = document.createElement("section");
+    // create div to house list of characters
+    const mainDiv = document.createElement('div');
+    section.appendChild(mainDiv)
+    mainDiv.textContent = `Name: ${n}, Race: ${r}, Class: ${c}, Gender: ${g}`;
+    return section;
+}
+
+
+const charactersDiv = document.querySelector('div#exampleCharacters')
+
+for (char of characters) {
+    const section = showCharacters(char.name, char.race, char.class, char.gender);
+    charactersDiv.appendChild(section);
+}
